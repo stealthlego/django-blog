a54003b3beebbfbe758382f5f25a1aea3b6ce90d
stealthlego <walexanderanderson@gmail.com>
2021-10-28 21:35:06 -0700

    Applied black formatting

--- mysite/blogging/urls.py
+++ mysite/blogging/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from blogging.views import BlogListView, BlogDetailView
 
 urlpatterns = [
-    path('', BlogListView.as_view(), name="blog_index"),
+    path("", BlogListView.as_view(), name="blog_index"),
     # path('', stub_view, name="blog_index"),
-    path('posts/<int:pk>/', BlogDetailView.as_view(), name="blog_detail"),
+    path("posts/<int:pk>/", BlogDetailView.as_view(), name="blog_detail"),
 ]
