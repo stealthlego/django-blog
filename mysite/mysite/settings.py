5ae657975b9aea9fbbc644c1756b1b92d8fb5815
stealthlego <walexanderanderson@gmail.com>
2021-10-28 22:44:57 -0700

    updated settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -125,4 +125,4 @@ LOGIN_REDIRECT_URL = "/"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "mysite", "static")
